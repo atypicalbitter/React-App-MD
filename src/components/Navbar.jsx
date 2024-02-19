1f19df0895d9bd79549f1f9c3e332c82f1f5ee9f
Matt Dudman <mattdudman95@googlemail.com>
2024-02-19 23:23:15 +0000

    Styling added in to the navbar. Not finished but is now functional and now is at the top of the page.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import { useState }  from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './Navbar.css'
+import '../App.css'
 
 function Mynav() {
+    const [activeLink, setActiveLink] = useState('home')
   return (
     <div>
       <Navbar bg="dark" data-bs-theme="dark">
@@ -11,9 +13,15 @@ function Mynav() {
           <Navbar.Brand href="#home">Matt Dudman</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#aboutme">About Me</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
+          <span className="navbar-text">
+            <div className='social-icon'>
+                <a href='https://www.linkedin.com/in/matt-dudman95/'><img src="" alt="" /></a> 
+            </div>
+          </span>
         </Container>
       </Navbar>
       </div>
