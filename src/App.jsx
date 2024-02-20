bc4e6ba04a37a7faa42a1cf96ad26e4f81fc4a66
Matt Dudman <mattdudman95@googlemail.com>
2024-02-20 00:04:39 +0000

    Header.jsx created and added into repo. Bootstrap col sizing added in along with some other formatting.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Mynav from './components/Navbar'
+import Header from './components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
 <Mynav />
+<Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Col, Container, Row } from "react-bootstrap"
+
+export const Header = () => {
+    return (
+<section className="header" id="home">
+    <Container>
+        <Row className="align-items-center">
+            <Col xs={12} md={6} xl={7}>
+            </Col>
+        </Row>
+    </Container>
+</section>
+    )
+}
